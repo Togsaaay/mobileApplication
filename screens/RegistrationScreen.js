299c1bbbf25334dfe8562f54a1cd00c10cc464e5
trixieln <navacilla.trixie14@gmail.com>
2024-09-13 13:40:20 +0800

    Registration Screen

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -10,6 +10,7 @@ import {
   Pressable,
 } from "react-native";
 import React, { useState } from "react";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 const LoginScreen = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -21,43 +22,45 @@ const LoginScreen = () => {
       </View>
       <Text style={styles.textEdit}>Sign Up!</Text>
       <Text style={styles.textHeading}>Create your account here!</Text>
-      <View style={styles.inputContainer}>
-        <View style={styles.inputSquare}>
-          <Image
-            style={styles.emailPic}
-            source={require("../assets/email.png")}
-          />
-          <TextInput style={styles.inputs} placeholder="Email" />
-        </View>
-        <View style={styles.inputSquare}>
-          <Image
-            style={styles.passPic}
-            source={require("../assets/pass.png")}
-          />
-          <TextInput style={styles.inputs} placeholder="Password" />
-        </View>
+      <View style={styles.emailcontainer}>
+        <FontAwesome
+          name="envelope"
+          size={15}
+          color="black"
+          style={styles.iconStyle}
+        />
+        <TextInput style={styles.emailTextInput} placeholder="Email" />
       </View>
-      <View style={styles.forgotContainer}>
-        <TouchableOpacity
-          onPress={() => {
-            console.log("Forgot Password pressed");
-          }}
-        >
-          <Text style={styles.textForgot}>Forgot Password?</Text>
-        </TouchableOpacity>
+      <View style={styles.namecontainer}>
+        <FontAwesome
+          name="user"
+          size={20}
+          color="black"
+          style={styles.iconStyle}
+        />
+        <TextInput style={styles.nameTextInput} placeholder="Name" />
       </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.forButton}
-          onPress={() => {
-            console.log("Login button pressed");
-            setModalVisible(true);
-          }}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
+      <View style={styles.numbercontainer}>
+        <FontAwesome
+          name="phone"
+          size={20}
+          color="black"
+          style={styles.iconStyle}
+        />
+        <TextInput style={styles.numberTextInput} placeholder="Phone Number" />
       </View>
-
+      <Text style={styles.dataCondition}>By signing up, you agree to our Terms & condition and Privacy Policy</Text>
+      {}
+      <TouchableOpacity
+        style={styles.forButton}
+        onPress={() => {
+          console.log("Login button pressed");
+          setModalVisible(true);
+        }}
+      >
+        <Text style={styles.buttonText}>Continue</Text>
+      </TouchableOpacity>
+      {}
       <Modal
         transparent={true}
         visible={modalVisible}
@@ -67,7 +70,9 @@ const LoginScreen = () => {
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalView}>
-            <Text style={styles.modalText}>Char, naka login siya baii! HAHAHAHAHAHAHAH</Text>
+            <Text style={styles.modalText}>
+              Char, naka sign up siya baii! HAHAHAHAHAHAHAH
+            </Text>
             <Pressable
               style={styles.buttonOk}
               onPress={() => setModalVisible(false)}
@@ -77,7 +82,7 @@ const LoginScreen = () => {
           </View>
         </View>
       </Modal>
-
+{/*
       <View style={styles.line}></View>
       <View style={styles.socMedContainer}>
         <TouchableOpacity
@@ -101,151 +106,158 @@ const LoginScreen = () => {
           />
         </TouchableOpacity>
       </View>
-    </View>
+*/}
+    </View> 
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    backgroundColor: "white",
-  },
-  topImageContainer: {
-    width: "100%",
-    height: "30%",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  topImage: {
-    width: "100%",
-    height: "100%",
-  },
-  textEdit: {
-    fontSize: 60,
-    fontWeight: "bold",
-    marginTop: 20,
-  },
-  inputContainer: {
-    flexDirection: "column",
-    alignItems: "center",
-    height: 140,
-    width: "100%",
-    marginTop: 80,
-    gap: 30,
-  },
-  inputs: {
-    height: 50,
-    width: "90%",
-    padding: 10,
-    fontSize: 19,
-  },
-  inputSquare: {
-    width: "80%",
-    flexDirection: "row",
-    alignItems: "center",
-    borderWidth: 1,
-    borderRadius: 10,
-    paddingLeft: 5,
-    gap: 2,
-  },
-  emailPic: {
-    width: 30,
-    height: 20,
-  },
-  passPic: {
-    width: 25,
-    height: 30,
-  },
-  forgotContainer: {
-    width: "80%",
-    alignItems: "flex-end",
-  },
-  textForgot: {
-    fontSize: 20,
-    color: "blue",
-  },
-  buttonContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "5%",
-    marginTop: 80,
-  },
-  forButton: {
-    width: "80%",
-    backgroundColor: "#4E9AF2",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 1,
-    borderRadius: 10,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  line: {
-    width: "80%",
-    height: 1,
-    backgroundColor: "gray",
-    marginTop: 80,
-  },
-  socMedPic: {
-    width: 60,
-    height: 50,
-  },
-  fbPic: {
-    width: 55,
-    height: 50,
-  },
-  textHeading: {
-    color: "black",
-    fontSize: 20,
-  },
-  socMedContainer: {
-    flexDirection: "row",
-    gap: 50,
-    paddingTop: 30,
-  },
-
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-  },
-  modalView: {
-    width: '100%',
-    height: '30%',
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    flexDirection: 'column',
-    alignItems: "center",
-    justifyContent: 'center',
-    gap: 50
-  },
-  buttonOk: {
-    width: '80%',
-    height: '20%',
-    borderRadius: 10,
-    backgroundColor: "#2196F3",
-    borderWidth: 1
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 30,
-    textAlign: 'center',
-    marginTop: 15,
-  },
-  modalText: {
-    textAlign: "center",
-    fontSize: 30,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      alignItems: "center",
+      backgroundColor: "white",
+    },
+    topImageContainer: {
+      width: "100%",
+      height: "30%",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    topImage: {
+      width: "100%",
+      height: "100%",
+    },
+    textEdit: {
+      fontSize: 60,
+      fontWeight: "bold",
+      marginTop: 20,
+    },
+    textHeading: {
+      color: "black",
+      fontSize: 20,
+    },
+    emailcontainer: {
+      borderBlockColor: "gray",
+      flexDirection: "row",
+      marginHorizontal: 50,
+      marginVertical: 50,
+      marginTop: 30,
+      borderBottomWidth: 1, 
+      height: 50,
+      alignItems: "center",    
+    },
+    emailTextInput: {
+      height: 50,
+      width: "90%",
+      padding: 10,
+      fontSize: 19,
+    },
+    namecontainer: {
+      borderBlockColor: "gray",
+      flexDirection: "row",
+      marginHorizontal: 50,
+      marginVertical: -20,
+      marginTop: -40,
+      height: 50,
+      borderBottomWidth: 1, 
+      alignItems: "center",
+    },
+    nameTextInput: {
+      height: 50,
+      width: "90%",
+      padding: 10,
+      fontSize: 19,
+    },
+    numbercontainer: {
+      borderBlockColor: "gray",
+      flexDirection: "row",
+      marginHorizontal: 50,
+      marginVertical: 50,
+      marginTop: 30,
+      height: 50,
+      borderBottomWidth: 1, 
+      alignItems: "center",
+    },
+    numberTextInput: {
+      height: 50,
+      width: "90%",
+      padding: 10,
+      fontSize: 19,
+    },
+    dataCondition:{
+      fontSize: 15,
+      textAlign: "center",
+      width: "80%",
+      marginVertical: -45,
+      color: "#0d81ad",
+      alignSelf: 'center'
+    },
+    forButton: {
+      width: "80%",
+      backgroundColor: "#4E9AF2",
+      paddingVertical: 10,
+      justifyContent: "center",
+      alignItems: "center",
+      borderWidth: 1,
+      borderRadius: 10,
+      marginTop: 80,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    /*line: {
+      width: "80%",
+      height: 1,
+      backgroundColor: "gray",
+      marginTop: 30,
+    },
+    socMedContainer: {
+      flexDirection: "row",
+      gap: 50,
+      paddingTop: 10,
+    },
+    /*socMedPic: {
+      width: 50,
+      height: 50,
+    },
+    /*fbPic: {
+      width: 55,
+      height: 50,
+    }, */
+    modalContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    modalView: {
+      width: "80%",
+      padding: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    modalText: {
+      textAlign: "center",
+      fontSize: 20,
+      marginBottom: 20,
+    },
+    buttonOk: {
+      backgroundColor: "#2196F3",
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 10,
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      fontSize: 16,
+      textAlign: "center",
+    },
+  });
